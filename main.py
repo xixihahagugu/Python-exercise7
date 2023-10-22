91864f07ac696f3dffc2b99f0bc249da859b1669
xixihahagugu <Mengna.Deng@metropolia.fi>
2023-10-22 22:14:28 +0800

    done 7.1

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-# Exercises Module 5.4
+# 7.1
+month_of_the_year=("spring","spring","spring","summer","summer","summer","autumn","autumn","autumn","winter","winter","winter",)
+month_number=int(input("Enter the month number(1-12):"))
+x=month_of_the_year[month_number-1]
+print(f"month number {month_number} is {x}.")
 
-cities = []
-
-for i in range(5):
-    city = input("Enter a city name: ")
-    cities.append(city)
-
-print("List of cities:")
-for city in cities:
-    print (city)
