a9b10daa00af1cdd62dd9b7150cffc1d0c04133c
xixihahagugu <Mengna.Deng@metropolia.fi>
2023-10-22 17:39:31 +0800

    done for exercise 3.1

---
+++ main.py
@@ -0,0 +1,4 @@
+length = float(input("Enter the length of a zander (in cm)"))
+if length < 42:
+    x = 42-length
+    print(f"Release the fish back into the lake,below the size limit the caught fish was {x}")
