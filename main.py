9b8d8d272a3410d9bc48a20ba9aa8702361589f1
xixihahagugu <Mengna.Deng@metropolia.fi>
2023-10-22 20:35:04 +0800

    done with exercise4.3

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-# Exercise 4.2
+"""
+Write a program that asks the user to enter numbers until they
+enter an empty string to quit. Finally, the program prints out
+the smallest and largest number from the numbers it received.
+"""
 
-"""
-Write a program that converts inches to centimeters until the user inputs a
-negative value. Then the program ends.
-"""
+
+smallest = None
+largest = None
 
 while True:
-    inches = float(input("Enter inches: "))
 
-    if inches > 0:
-        centimeters = inches * 2.54
-        print(f"{inches} inches is {centimeters:.2f} centimeters")
-    else:
-        print("Negative values, Program ends")
+    user = input("Enter a number (or press Enter to quit): ")
+
+    if user == "":
         break
+
+    try:
+        number = float(user)
+
+        if smallest is None or number < smallest:
+            smallest = number
+        if largest is None or number > largest:
+            largest = number
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+print(f"The smallest number you received is {smallest} and the largest one is {largest}")
 
