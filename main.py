9a098d65481b843e4c23a7e027d54c1f04ffd157
xixihahagugu <Mengna.Deng@metropolia.fi>
2023-10-22 21:17:12 +0800

    done with exercise5.1

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-# Exercises Module 5.4
+# Exercises Module 5.1
 
-cities = []
+import random
 
-for i in range(5):
-    city = input("Enter a city name: ")
-    cities.append(city)
+def rolldices():
 
-print("List of cities:")
-for city in cities:
-    print (city)
+    num_dices = int(input("How many dices do you want to run? "))
+
+    total = 0
+
+    for dice in range(num_dices):
+
+        roll = random.randint(1, 6)
+        total += roll
+
+    # Prints out the sum of the numbers
+    print(f"Total numbers of the dice rolls are {total} ")
+
+
+rolldices()
